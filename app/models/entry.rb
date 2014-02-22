f02ac88b43b1901ac13cd7f02cb284e99adfbc74
Laurent Curau <laurentcurau@gmail.com>
2014-02-22 16:04:13 +0000

    Set up model associations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,4 @@
 class Entry < ActiveRecord::Base
   attr_accessible :author, :content, :feed_id, :image, :published_on, :summary, :title, :url
+  belongs_to :feed
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,4 @@
 class Feed < ActiveRecord::Base
   attr_accessible :last_updated, :publisher_url, :title, :url
+  has_many :entries
 end
