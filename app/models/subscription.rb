96dfb760f84eadac7d011925ff1501ed60307b42
Laurent Curau <laurentcurau@gmail.com>
2014-02-23 14:25:50 +0000

    Create MVC for subscription

---
+++ app/models/subscription.rb
@@ -0,0 +1,5 @@
+class Subscription < ActiveRecord::Base
+  attr_accessible :user_id, :feed_id
+  belongs_to :user
+  belongs_to :feed
+end
---
+++ db/migrate/20140222152922_create_subscriptions_table.rb
@@ -0,0 +1,10 @@
+class CreateSubscriptionsTable < ActiveRecord::Migration
+  def change
+    create_table :subscriptions do |t|
+      t.integer :user_id
+      t.integer :feed_id
+
+      t.timestamps
+    end
+  end
+end
