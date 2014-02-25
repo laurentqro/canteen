34f69eb38eaff92a68da1dfab9e93d99b4436379
Andy Born <andybornx@gmail.com>
2014-02-25 14:51:56 +0000

    fixes subscriptions view, adds before filter to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :setup_feed_instance_variable
+
+  def setup_feed_instance_variable
+    @feed = Feed.new
+  end
  
   
 end
